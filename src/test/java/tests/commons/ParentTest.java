4acb5ee2d89f53b9954284817b0265c483139dd8
vipul <sharma.vipul.999@gmail.com>
2018-10-21 19:03:35 +0530

    Changes for Selenium Grid

--- src/test/java/tests/commons/ParentTest.java
+++ src/test/java/tests/commons/ParentTest.java
@@ -6,25 +6,33 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 public class ParentTest {
-	
-	public static WebDriver driver;
+	public RemoteWebDriver driver;
 	private Properties properties = null;
 	
 	@BeforeClass
 	public void parentSetUp() throws MalformedURLException {
 		System.setProperty("webdriver.chrome.driver", "E:\\Software\\chromedriver.exe");
-		driver = new ChromeDriver();
+//		driver = new ChromeDriver();
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setBrowserName("chrome");
+		capabilities.setPlatform(Platform.WINDOWS);
+		capabilities.setJavascriptEnabled(true);
+		driver = new RemoteWebDriver(new URL("http://192.168.0.3:4444/wd/hub"), capabilities);
 		driver.manage().window().maximize();
 		driver.get("https://www.ajio.com");
 	}
