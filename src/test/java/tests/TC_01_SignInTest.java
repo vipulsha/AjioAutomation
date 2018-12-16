4fcbd134df16591afd8e7190f488296fbc8abf34
vipul <sharma.vipul.999@gmail.com>
2018-12-16 14:21:52 +0530

    adding step for screenshot

--- src/test/java/tests/TC_01_SignInTest.java
+++ src/test/java/tests/TC_01_SignInTest.java
@@ -4,7 +4,10 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import atu.testng.reports.ATUReports;
+import atu.testng.reports.logging.LogAs;
 import atu.testng.reports.utils.Utils;
+import atu.testng.selenium.reports.CaptureScreen;
+import atu.testng.selenium.reports.CaptureScreen.ScreenshotOf;
 import pages.HomePage;
 import pages.UserHomePage;
 import tests.commons.ParentTest;
@@ -13,12 +16,15 @@ public class TC_01_SignInTest extends ParentTest {
 	@Test
 	public void signInTest() {
 		ATUReports.setAuthorInfo("Vipul Sharma", Utils.getCurrentTime(),"1.0");
-		
-		HomePage homePage = new HomePage(driver);
-		homePage.signIn(getData("emailId"), getData("password"));
-		UserHomePage userHomePage = new UserHomePage(driver);
-		String welComeMessage = userHomePage.getWelComeMessage();
-		String userName = getData("userName");
-		Assert.assertTrue(welComeMessage.contains(userName), "Failed: User is not logged in");
+		try {
+			HomePage homePage = new HomePage(driver);
+			homePage.signIn(getData("emailId"), getData("password"));
+			UserHomePage userHomePage = new UserHomePage(driver);
+			String welComeMessage = userHomePage.getWelComeMessage();
+			String userName = getData("userName");
+			Assert.assertTrue(welComeMessage.contains(userName), "Failed: User is not logged in");	
+		} catch (Exception e) {
+			ATUReports.add("Failed", LogAs.FAILED, new CaptureScreen(ScreenshotOf.BROWSER_PAGE));
+		}
 	}
 }
